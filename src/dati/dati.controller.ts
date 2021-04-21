c16bc501b93ef30eca1445c4192257770776ca95
chba78 <chba78@gmail.com>
2021-04-21 07:04:26 +0200

    RottaAnnidata

--- src/dati/dati.controller.ts
+++ src/dati/dati.controller.ts
@@ -3,7 +3,16 @@ import { Controller, Get, Req, Request } from '@nestjs/common';
 @Controller('dati')
 export class DatiController {
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'visualizzazione elenco dati';
+  findAll(@Req() request: Request): {} {
+    return [
+      {datos: 'Primoser', daton: 111, datod: new Date('2011-01-01T01:01:01.932Z'), datob: true},
+      {datos: 'Secondoser', daton: 222, datod: new Date('2011-01-02T01:01:02.932Z'), datob: false},
+      {datos: 'Terzoser', daton: 333, datod: new Date('2011-01-03T01:01:03.932Z'), datob: true},
+    ];
+  }
+
+  @Get('mostra')
+  mostra(@Req() request: Request): string {
+    return 'Questa è la mostra';
   }
 }
